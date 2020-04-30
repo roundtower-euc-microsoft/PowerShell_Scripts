ed99d21ca75a07b3f5ecc5e245c5869df5b15739
Corey St. Pierre <corey.stpierre@roundtower.com>
2020-04-30 09:33:49 -0500

    Starting Of

---
+++ Production/Microsoft Exchange/Cleanup_Old_IIS_Logs.ps1
@@ -0,0 +1,19 @@
+﻿Import-Module WebAdministration  
+  
+#Maximum age in days of files to be deleted  
+$logfileMaxAge = 2  
+foreach($website in $(Get-Website))  
+{  
+    #Get log folder for current website  
+    $folder="$($website.logFile.directory)\W3SVC$($website.id)".replace("%SystemDrive%",$env:SystemDrive)  
+    #Get all log files in the folder  
+    $files = Get-ChildItem $folder -Filter *.log  
+  
+    foreach($file in $files){  
+        if($file.LastWriteTime -lt (Get-Date).AddDays(-1*$logfileMaxAge)){  
+            #Remove fie older than logfileMaxAge days  
+            Remove-Item $file.FullName  
+  
+        }  
+    }  
+}  
